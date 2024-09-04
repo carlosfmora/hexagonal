651c438e368e3317eda3aed43f206db35ed7028e
fernandomora <carlos.mora@appgate.com>
2024-09-04 12:51:58 -0500

    rename module

---
+++ jakartavalidations/src/main/java/com/example/jakartavalidations/JakartavalidationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.jakartavalidations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakartavalidationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakartavalidationsApplication.class, args);
+    }
+
+}
